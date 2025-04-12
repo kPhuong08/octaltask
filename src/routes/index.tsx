d6c2b0cdaa3cd34ca659acb02a55341670fa3cbf
Vo Tran Phi <songokupkj@gmail.com>
2025-04-12 14:43:19 +0700

    feat(ot-26): add pass reco routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "@/pages/Login";
 import Signup from "@/pages/Signup";
+import PasswordRecovery from "@/pages/PasswordRecovery";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/password_recovery" element={<PasswordRecovery />} />
       <Route path="/" element={<Navigate to="/login" replace />} />
     </Routes>
   );
